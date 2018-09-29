67d9641d2faf591675ebf70744afd3b06aef3067
AndyCJ <andycj@csharpdeveloper.co.uk>
2018-09-29 12:47:46 +0100

    Added IrcChannelMessageMessage message bus message. Wired up Bot to produce IrcChannelMessageMessages. Wired up User resolving based on IrcHost.

---
+++ src/SpikeCore/SpikeCore.Messages/SpikeCore.Messages/IrcChannelMessageMessage.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SpikeCore.Messages
+{
+    public class IrcChannelMessageMessage
+    {
+        public IdentityUser<string> IdentityUser { get; set; }
+        public string ChannelName { get; set; }
+        public string UserName { get; set; }
+        public string UserHostName { get; set; }
+        public string Text { get; set; }
+    }
+}

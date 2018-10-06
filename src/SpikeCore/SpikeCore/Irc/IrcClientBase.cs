aaa76482194f4e47f92b0939db080cc761d23c58
AndyCJ <andycj@csharpdeveloper.co.uk>
2018-10-06 20:14:07 +0100

    Created IrcClientBase for shared implimentation between IIrcClients.

---
+++ src/SpikeCore/SpikeCore/Irc/IrcClientBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpikeCore.Irc
+{
+    public abstract class IrcClientBase: IIrcClient
+    {
+        protected IEnumerable<string> _channelsToJoin;
+
+        public virtual Action<string> MessageReceived { get; set; }
+        public virtual Action<ChannelMessage> ChannelMessageReceived { get; set; }
+
+        public virtual void Connect(string host, int port, string nickname, IEnumerable<string> channelsToJoin)
+            => _channelsToJoin = channelsToJoin;
+
+        public abstract void SendChannelMessage(string channelname, string message);
+    }
+}

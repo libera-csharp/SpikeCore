89fa8411ea725aa09f83565e0d673e7106df57b2
AndyCJ <andycj@csharpdeveloper.co.uk>
2018-10-27 10:52:41 +0100

    Created EF migration LoginMetaData.

---
+++ src/SpikeCore/SpikeCore/Data/Migrations/20181027095047_LoginMetaData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpikeCore.Data.Migrations
+{
+    public partial class LoginMetaData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MetaData",
+                table: "AspNetUserLogins",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MetaData",
+                table: "AspNetUserLogins");
+        }
+    }
+}

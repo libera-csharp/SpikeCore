d7c5362293a52b24aae2e7a9c6293816c859f898
Greg F <kog@epiphanic.org>
2018-10-31 00:21:12 -0700

    Preliminary support for basic role-based authorization
    * Adds a the `Roles` property to `SpikeCoreUser`
        * This is `NotMapped`, and is fed into the object in our `IrcConnection`, which looks up the roles for the user
    
    * `IrcConnection` now looks up any available roles for a user via `GetRolesAsync` on the user manager
    * `ModuleBase` has a new virtual method called `AccessAllowed`
        * This gets passed a `SpikeCoreUser` from the message context
        * By default anyone with a user that has at least 1 role can activate a module
        * More sensitive modules (ops, admin), can override this to look for more explicit permissions/conditions
    
    * Misc cleanup: the `Configuration` property of `ModuleBase` now has a private get.
        * We need the public set for property injection, but nothing else reads this
    
    * Wire up `IdentityRole` in Startup while we're setting up the rest of the auth
        * Be careful, order is important (although it looks like a standard builder)
    
    * Adds 3 roles to the DB:
         * Regular - lowest privilege level, granted to all regulars for "public" commands
         * Op - will be able to use things like kick/ban etc
         * Admin - will be able to do things like shutdown, join, part etc.
         * Assigns all three roles to both AspNetUsers for now

--- src/SpikeCore/SpikeCore/Data/Models/SpikeCoreUser.cs
+++ src/SpikeCore/SpikeCore/Data/Models/SpikeCoreUser.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace SpikeCore.Data.Models
 {
-    public class SpikeCoreUser : IdentityUser { }
+    public class SpikeCoreUser : IdentityUser
+    {
+        [NotMapped] public IEnumerable<string> Roles { get; set; }
+    }
 }
--- src/SpikeCore/SpikeCore/Modules/ModuleBase.cs
+++ src/SpikeCore/SpikeCore/Modules/ModuleBase.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Foundatio.Messaging;
+using SpikeCore.Data.Models;
 using SpikeCore.Irc.Configuration;
 using SpikeCore.MessageBus;
 
@@ -18,14 +19,14 @@ namespace SpikeCore.Modules
         public virtual IEnumerable<string> Triggers => new List<string> {Name};
 
         public IMessageBus MessageBus { private get; set; }
-        public ModuleConfiguration Configuration { get; set; }
+        public ModuleConfiguration Configuration { private get; set; }
 
         // TODO [Kog 10/06/2018] : work in access checks etc.
         public Task HandleMessageAsync(IrcChannelMessageMessage message, CancellationToken cancellationToken)
         {
             return Triggers.Any(trigger =>
                 message.Text.StartsWith(Configuration.TriggerPrefix + trigger,
-                    StringComparison.InvariantCultureIgnoreCase) && message.IdentityUser != null)
+                    StringComparison.InvariantCultureIgnoreCase) && AccessAllowed(message.IdentityUser))
                 ? HandleMessageAsyncInternal(message, cancellationToken)
                 : Task.CompletedTask;
         }
@@ -39,5 +40,11 @@ namespace SpikeCore.Modules
 
         protected Task SendMessagesToChannel(string channelName, IEnumerable<string> messages)
             => MessageBus.PublishAsync(new IrcSendChannelMessage(channelName, messages));
+
+        protected virtual bool AccessAllowed(SpikeCoreUser user)
+        {
+            // By default a command can be run by any known user with at least one role.
+            return null != user && user.Roles.Any();
+        }
     }
 }

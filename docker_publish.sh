3e39aaaf26b9d99ed2714ab4f0eda009bdfeea1c
Greg <kog@epiphanic.org>
2020-07-31 01:59:42 -0700

    Remove Travis CI tooling (#51)
    * Remove `.travis.yml` - we now use GitHub Actions
    * Remove `docker_publish.sh`, GitHub Actions has an action for this

--- docker_publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker build -t spike_core .
-docker tag spike_core freenodecsharp/spike_core:latest
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push freenodecsharp/spike_core:latest
